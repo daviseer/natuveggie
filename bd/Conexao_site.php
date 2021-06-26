4dac498818f8d4bea41d0d5dbcbc0298dcf88e6e
Davi <davi.seer@gmail.com>
2021-06-25 23:17:08 -0300

    conexao

--- bd/Conexao_site.php
+++ bd/Conexao_site.php
@@ -2,13 +2,13 @@
 
 class Conexao extends PDO {
 
-    private $banco = 'natuveggie';
-    private $user = 'root';
-    private $senha = '';
-
+    private $banco = 'seerde54_natuveggie';
+    private $user = 'seerde54_mysql';
+    private $senha = 'Dinhoechi@12';
+    private $host = '108.167.132.245';
 
     function __construct() {
-        $dsn = 'mysql:host=localhost;dbname=' . $this->banco;
+        $dsn = 'mysql:host='.$this->host.';dbname=' . $this->banco;
         $options = [
             PDO::ATTR_PERSISTENT => true,
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
